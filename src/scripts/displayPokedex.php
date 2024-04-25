503d0f34b061bb84e636b2bcdd6e7beab2ed1b0d
bgirot <balthazar.girot@gmail.com>
2024-04-25 09:49:46 +0200

    small style update, still bad

--- src/scripts/displayPokedex.php
+++ src/scripts/displayPokedex.php
@@ -7,8 +7,9 @@ function displayPokedex($pokedex) {
         echo '<li id="card-' .$value["id_pokemon"]. '" class="pokemon-card">';
 
         echo '<span class="pokemon-id">#'. $value["id_pokemon"] .'</span>';
-        echo '<img id="img-'.$value["id_pokemon"].'" class="pokemon-img" src="' . $value["image_path"] . '" alt=" '.$value["nom"].'">';
+        echo '<a class="btn" href=""><img id="img-'.$value["id_pokemon"].'" class="pokemon-img" src="' . $value["image_path"] . '" alt=" '.$value["nom"].'"></a>';
         echo '<span class="pokemon-name">'. $value["nom"] .'</span>';
+        echo '<a class="btn see-more-btn" href="">Plus d\'infos</a>';
         
 
         echo '</li>';
