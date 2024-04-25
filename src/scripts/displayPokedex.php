5d2e1fb0683d8a9e4a97ddbaf4e74e39c9536e30
bgirot <balthazar.girot@gmail.com>
2024-04-25 09:40:43 +0200

    new getAverageColor function

--- src/scripts/displayPokedex.php
+++ src/scripts/displayPokedex.php
@@ -4,12 +4,13 @@ function displayPokedex($pokedex) {
     echo '<ul class = "pokedex-wrapper">';
 
     foreach($pokedex as $key => $value) {
-        echo '<li class="pokemon-card">';
+        echo '<li id="card-' .$value["id_pokemon"]. '" class="pokemon-card">';
 
-        echo '<span class="pokemon-id"> # ' .$value["id_pokemon"]. '</span>';
-        echo '<img src=" '.$value["image_path"].' " alt=" '.$value["nom"].' ">';
-        echo '<span class="pokemon-name">'.$value["nom"].'</span>';
+        echo '<span class="pokemon-id">#'. $value["id_pokemon"] .'</span>';
+        echo '<img id="img-'.$value["id_pokemon"].'" class="pokemon-img" src="' . $value["image_path"] . '" alt=" '.$value["nom"].'">';
+        echo '<span class="pokemon-name">'. $value["nom"] .'</span>';
         
+
         echo '</li>';
     }
 
--- src/scripts/front.js
+++ src/scripts/front.js
@@ -1,6 +1,5 @@
 // Dropdown menu manager
 window.onclick = function(event) {
-    console.log(event.target);
 
     let navButton = document.getElementById('nav-btn');
     let navMenu = document.getElementById('nav-menu');
@@ -19,8 +18,6 @@ window.onclick = function(event) {
 
     } else if (! navMenu.contains(event.target)) {
 
-        console.log("ok");
-
         if (navMenu.classList.contains('active')) {
 
             navMenu.classList.remove('active');
@@ -29,4 +26,58 @@ window.onclick = function(event) {
 
     }
 
+}
+
+// To generate the color of the pokemon card in the pokedex page
+
+// Calculates the average color of the image and the average color of the most
+// comon colors
+function setMainColorToBackground(id) {
+    let img = document.getElementById("img-"+id);
+    let card = document.getElementById("card-"+id);
+
+    let blockSize = 5,              // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+
+    if (!context) {
+        return defaultRGB;
+    }
+
+    height = canvas.height = img.naturalHeight || img.offsetHeight || imgEl.height;
+    width = canvas.width = img.naturalWidth || img.offsetWidth || imgEl.width;
+
+    context.drawImage(img, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */
+        return defaultRGB;
+    }
+
+    length = data.data.length;
+
+    while ( (i += blockSize * 4) < length ) {
+
+        ++count;
+
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+    
+
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+
+    card.style.background = "rgb(" + rgb.r + "," + rgb.g  + "," + rgb.b + ")";
 }
